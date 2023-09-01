c2321bb44a3a291c9f544e2950215dff1c913184
Win11 <huxt-huxt@yandex.ru>
2023-09-01 20:01:26 +0300

    some_progress

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,16 @@
 import './App.css';
 import Header from '../Header/Header';
+import * as api from '../../utils/Api';
+import React from 'react';
 
 function App() {
+
+  React.useEffect(() => {
+    api.getEvents()
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  }, [])
+
   return (
     <div className="page">
       <Header />
---
+++ src/utils/Api.js
@@ -0,0 +1,16 @@
+export const BASE_URL = 'http://127.0.0.1:80';
+
+export const getEvents = () => fetch(`${BASE_URL}/letters`, {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  }
+})
+  .then(handleResponse);
+
+const handleResponse = (response) => {
+  if (response.ok) {
+    return response.json();
+  }
+  return Promise.reject(response.status);
+};
